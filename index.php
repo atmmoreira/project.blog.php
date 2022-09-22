06acb1807008d0daeeb6c359aa28b385edcd1eae
Anderson Martins <atmmoreira.rj@gmail.com>
2022-09-22 08:26:32 -0300

    feature: content structure initial

--- index.php
+++ index.php
@@ -12,6 +12,54 @@
 
 <body>
 
+  <nav class="navbar navbar-dark navbar-expand-lg bg-dark ">
+    <div class="container">
+      <a class="navbar-brand" href="#">BLOG PHP</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <main class="my-5">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-3 mb-3">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <small>
+                <span class="badge text-bg-dark">Java</span>
+                <span class="badge text-bg-dark">OOP</span>
+                <span class="badge text-bg-dark">Design Patterns</span>
+              </small>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </main>
+
+  <footer class="bg-dark text-white py-2">
+    <div class="container small text-center">
+      <small>© 2022 Blog PHP, <a class="link-light text-decoration-none" href="https://www.atmm.dev">ATMM.dev</a>.</small>
+    </div>
+  </footer>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
